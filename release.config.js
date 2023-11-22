4a3eab5bbc7e9cb08b615cd90e3aa733a887c644
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-11-22 14:07:10 +0700

    feat: add changelog, git plugins

--- release.config.js
+++ release.config.js
@@ -1,4 +1,12 @@
 /** @type {import('semantic-release').GlobalConfig} */
 export default {
   branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    "@semantic-release/github",
+  ],
 };
