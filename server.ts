6e325e458817a2fe5c824a85d69db72996572a4b
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-11-22 15:14:15 +0700

    add error path

--- server.ts
+++ server.ts
@@ -12,6 +12,10 @@ fastify.get("/bye", async () => {
   return { bye: "world 2" };
 });
 
+fastify.get("/error", async () => {
+  throw new Error("something went wrong");
+});
+
 try {
   await fastify.listen({ port: 3000 });
 } catch (err) {
